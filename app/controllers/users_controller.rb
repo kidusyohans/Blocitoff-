09339672cf5abd7b847864a2667e4081cecf22f0
kidusyohans <kidusyohans@hotmail.com>
2015-09-08 05:14:10 +0000

    checkpoint-User-Profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
+  
+  
+  
   def show
+     @user = User.find(params[:id])
+     @tasks = @user.tasks.visible_to(current_user)
+     
   end
 
   def update
