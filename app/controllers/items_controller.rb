7d3345d4225ec45e53e4b1663cf00872ce3f79c1
kidusyohans <kidusyohans@hotmail.com>
2015-09-09 23:28:38 +0000

    checkpoint-Mark-To-Do-Items-as-Complete

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,4 +18,23 @@ class ItemsController < ApplicationController
 
   def update
   end
+  
+  def destroy
+     @task = Task.find(params[:post_id])
+     @item = @task.items.find(params[:id])
+     authorize @item
+
+     if @item.destroy
+       flash[:notice] = "Item was removed."
+       redirect_to [@task.item, @task]
+     else
+       flash[:error] = "Item couldn't be deleted. Try again."
+       redirect_to [@task.item, @task]
+     end
+
+     respond_to do |format|
+       format.html
+       format.js
+     end
+  end
 end
